62b3ac92e634d385cfe61cf63a3f1008f0bad172
Eduardo Quiñonez <okitoxo@gmail.com>
2021-03-06 20:13:49 -0500

    huion to only one screen

---
+++ wacom/config-huion-13.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Buttons
+# xsetwacom --set "Wacom Intuos BT S Pad pad" Button 1 "key tab"
+# xsetwacom --set "Wacom Intuos BT S Pad pad" Button 2 "key ctrl z"
+# xsetwacom --set "Wacom Intuos BT S Pad pad" Button 3 "key -"
+# xsetwacom --set "Wacom Intuos BT S Pad pad" Button 8 "key +"
+
+# Screen mapping
+xsetwacom set "Tablet Monitor stylus" MapToOutput HDMI-A-0
+# xsetwacom set "Wacom Intuos BT S Pen stylus" Area 0 0 15200 8550
+# xsetwacom set "Wacom Intuos BT S Pen eraser" Area 0 0 15200 8550
+# xsetwacom set "Wacom Intuos BT S Pen cursor" Area 0 0 15200 8550
+# xsetwacom set "Wacom Intuos BT S Pen eraser" Area 0 0 15200 8550
+# xsetwacom set "Wacom Intuos BT S Pen cursor" Area 0 0 15200 8550
+
